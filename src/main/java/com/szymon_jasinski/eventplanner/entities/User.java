f95c89db9f1d61a44249556014a7bb6cd13865d6
Szymon Jasiński <szg.jasinski@gmail.com>
2023-05-19 21:35:06 +0200

    Add User entity and corresponding DTO

---
+++ src/main/java/com/szymon_jasinski/eventplanner/entities/User.java
@@ -0,0 +1,18 @@
+package com.szymon_jasinski.eventplanner.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+}
