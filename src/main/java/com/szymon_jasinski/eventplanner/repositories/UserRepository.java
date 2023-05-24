b85ca5e8bb7bb55766879368db627a2c3a469367
Szymon Jasiński <szg.jasinski@gmail.com>
2023-05-24 19:52:49 +0200

    Add findByEmail method

---
+++ src/main/java/com/szymon_jasinski/eventplanner/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.szymon_jasinski.eventplanner.repositories;
+
+import com.szymon_jasinski.eventplanner.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
