dc687e8588664bd2180c1039b33da37e7b93fb7e
Szymon Jasiński <szg.jasinski@gmail.com>
2023-05-28 19:55:30 +0200

    feat: Add registering validation

--- src/main/java/com/szymon_jasinski/eventplanner/dtos/RegisterDTO.java
+++ src/main/java/com/szymon_jasinski/eventplanner/dtos/RegisterDTO.java
@@ -2,9 +2,16 @@ package com.szymon_jasinski.eventplanner.dtos;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class RegisterDTO {
+    @NotBlank
     private String name;
+    @Email
     private String email;
+    @NotEmpty
     private String password;
 }
