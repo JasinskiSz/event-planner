d9a41013d33c794c8f15200be7f819c9e986d1d1
Szymon Jasiński <szg.jasinski@gmail.com>
2023-06-07 22:58:10 +0200

    feat: Register new user via API endpoint

--- src/main/java/com/szymon_jasinski/eventplanner/controllers/UserController.java
+++ src/main/java/com/szymon_jasinski/eventplanner/controllers/UserController.java
@@ -1,11 +1,17 @@
 package com.szymon_jasinski.eventplanner.controllers;
 
+import com.szymon_jasinski.eventplanner.dtos.RegisterDTO;
 import com.szymon_jasinski.eventplanner.dtos.UserDTO;
+import com.szymon_jasinski.eventplanner.entities.User;
 import com.szymon_jasinski.eventplanner.services.UserService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @AllArgsConstructor
@@ -13,6 +19,20 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final ModelMapper modelMapper;
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> register(@RequestBody @Valid RegisterDTO registerDTO) {
+        User user = userService.create(modelMapper.map(registerDTO, User.class));
+        UserDTO createdUserDTO = modelMapper.map(user, UserDTO.class);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/api/v1/users/{id}")
+                .buildAndExpand(createdUserDTO.getId())
+                .toUri();
+        return ResponseEntity
+                .created(location)
+                .body(createdUserDTO);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> get(@PathVariable Long id) {
--- src/test/java/com/szymon_jasinski/eventplanner/controllers/UserControllerTest.java
+++ src/test/java/com/szymon_jasinski/eventplanner/controllers/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.szymon_jasinski.eventplanner.controllers;
 
+import com.szymon_jasinski.eventplanner.dtos.RegisterDTO;
 import com.szymon_jasinski.eventplanner.dtos.UserDTO;
 import com.szymon_jasinski.eventplanner.entities.User;
 import com.szymon_jasinski.eventplanner.services.UserService;
@@ -11,8 +12,14 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 class UserControllerTest {
@@ -28,8 +35,39 @@ class UserControllerTest {
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
+
+        // Set up mock Servlet request context
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(
+                new ServletRequestAttributes(request));
     }
 
+    @Test
+    public void register_ShouldReturnCreatedStatusAndLocationAndBody() {
+        // Prepare test data
+        RegisterDTO registerDTO = new RegisterDTO();
+        User user = new User();
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1L);
+        String basePath = "/api/v1/users";
+        URI location = URI.create(basePath + "/" + userDTO.getId());
+
+        // Mock the userService create method
+        when(userService.create(any(User.class))).thenReturn(user);
+
+        // Mock the modelMapper map method
+        when(modelMapper.map(registerDTO, User.class)).thenReturn(user);
+        when(modelMapper.map(user, UserDTO.class)).thenReturn(userDTO);
+
+        // Call the register method
+        ResponseEntity<UserDTO> responseEntity = userController.register(registerDTO);
+
+        // Verify the response
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals(location.getPath(), responseEntity.getHeaders()
+                .getLocation().getPath());
+        assertEquals(userDTO, responseEntity.getBody());
+    }
 
     @Test
     public void get_ShouldReturnOKStatusAndUserDTO() {
