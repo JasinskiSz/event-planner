8328842acd7553191b38139f000970ec0f20c9d5
Szymon Jasiński <szg.jasinski@gmail.com>
2023-06-14 02:02:04 +0200

    feat: Hash password on user creation
    Signed-off-by: Szymon Jasiński <szg.jasinski@gmail.com>

--- src/main/java/com/szymon_jasinski/eventplanner/services/UserService.java
+++ src/main/java/com/szymon_jasinski/eventplanner/services/UserService.java
@@ -4,14 +4,17 @@ import com.szymon_jasinski.eventplanner.entities.User;
 import com.szymon_jasinski.eventplanner.exceptions.ObjectNotFoundException;
 import com.szymon_jasinski.eventplanner.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User create(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
